9e4bb44344b41169363969cce16138cd3036dd45
cosmos1030 <dyk6208@gmail.com>
2022-11-13 09:52:22 +0000

    10816

---
+++ class2+/10816.py
@@ -0,0 +1,15 @@
+n = int(input())
+cards = list(map(int, input().split()))
+m = int(input())
+numbers = list(map(int, input().split()))
+
+mydic = dict.fromkeys(cards, 0)
+# print(mydic)
+for i in cards:
+    mydic[i] += 1
+# print(mydic)
+for i in numbers:
+    if i in mydic:
+        print(mydic[i], end=" ")
+    else:
+        print(0, end=" ")
