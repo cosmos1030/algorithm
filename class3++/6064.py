74787c58616c5475de27d299c1f128dc518f3275
cosmos1030 <dyk6208@gmail.com>
2022-12-04 08:17:40 +0000

    20221204

--- class3++/6064.py
+++ class3++/6064.py
@@ -1,6 +1,22 @@
-10 m+1 = 12 n + 11 = k = 12 (10p+q)+11
-k mod 10 = 1
-k mod 12 = 11
+# 10 m+1 = 12 n + 11 = k = 12 (10p+q)+11
+# k mod 10 = 1
+# k mod 12 = 11
 
-CRT(Chinese Remaider Theorem)
-n1 = 6, m1 = 
+# CRT(Chinese Remaider Theorem)
+
+import sys
+input = sys.stdin.readline
+
+t = int(input())
+for _ in range(t):
+    m, n, x, y = map(int, input().strip().split())
+    k = x
+    while(1):
+        if (k-y) % n == 0:
+            print(k)
+            break
+        else:
+            k += m
+        if k>m*n:
+            print(-1)
+            break
