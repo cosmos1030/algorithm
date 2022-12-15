437b50ca56835c87f620190af9c94e9837a976b3
cosmos1030 <dyk6208@gmail.com>
2022-12-15 12:24:14 +0000

    20221215

--- 클래스별/class3++/10026.py
+++ 클래스별/class3++/10026.py
@@ -1,5 +1,5 @@
 import sys
-import deque
+from collections import deque
 input = sys.stdin.readline
 n = int(input())
 paint = []
@@ -8,7 +8,46 @@ for i in range(n):
     temp = input().rstrip()
     paint.append(temp)
 
-queue_r = deque()
-queue_g = deque()
-queue_b = deque()
+dx = [0,0,1,-1]
+dy = [1,-1,0,0]
+visited = [[False for _ in range(n)] for _ in range(n)]
+result = 0
 
+for i in range(n):
+    for j in range(n):
+        queue = deque()
+        if visited[i][j] == False:
+            result += 1
+            color = paint[i][j]
+            queue.append([i,j])
+            visited[i][j] = True
+            while queue:
+                temp = queue.popleft()
+                for k in range(4):
+                    x = temp[0] + dx[k]
+                    y = temp[1] + dy[k]
+                    if x >=0 and x<n and y >=0 and y<n and (visited[x][y]==False) and (paint[x][y]==color):
+                        visited[x][y] = True
+                        queue.append([x,y])
+print(result)
+
+visited = [[False for _ in range(n)] for _ in range(n)]
+result = 0
+
+for i in range(n):
+    for j in range(n):
+        queue = deque()
+        if visited[i][j] == False:
+            result += 1
+            color = paint[i][j]
+            queue.append([i,j])
+            visited[i][j] = True
+            while queue:
+                temp = queue.popleft()
+                for k in range(4):
+                    x = temp[0] + dx[k]
+                    y = temp[1] + dy[k]
+                    if x >=0 and x<n and y >=0 and y<n and (visited[x][y]==False) and (paint[x][y]==color):
+                        visited[x][y] = True
+                        queue.append([x,y])
+print(result)
