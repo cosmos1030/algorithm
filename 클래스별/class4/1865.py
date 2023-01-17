2d35701468fb1ac746034256c82d68bde7ef6908
cosmos1030 <dyk6208@gmail.com>
2023-01-17 10:45:05 +0000

    20230117

---
+++ 클래스별/class4/1865.py
@@ -0,0 +1,36 @@
+import sys
+input = sys.stdin.readline
+INF = int(1234567891)
+
+
+def bf(start):
+    dis = [INF] * (n+1)
+    dis[start] = 0
+    for i in range(n):
+        for edge in edges:
+            cur = edge[0]
+            next_node = edge[1]
+            cost = edge[2]
+            if dis[next_node] > cost + dis[cur]:
+                dis[next_node] = cost + dis[cur]
+                if i == n-1:
+                    return True
+    return False
+
+t = int(input())
+
+for _ in range(t):
+    n,m,w = map(int, input().split())
+    edges = []
+    for _ in range(m):
+        s, e, t = map(int, input().split())
+        edges.append((s,e,t))
+        edges.append((e,s,t))
+    for _ in range(w):
+        s,e,t = map(int, input().split())
+        edges.append((s,e,-t))
+    isPossible = bf(1)
+    if isPossible:
+        print("YES")
+    else:
+        print('NO')
