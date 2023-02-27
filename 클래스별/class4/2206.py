4fc09ae00f0ecdc3e4736a9a902b0806aa18bf94
cosmos1030 <dyk6208@gmail.com>
2023-02-27 06:00:45 +0000

    20230227

--- 클래스별/class4/2206.py
+++ 클래스별/class4/2206.py
@@ -3,45 +3,35 @@ from collections import deque
 
 input = sys.stdin.readline
 
-n,m= map(int, input().split())
+n, m = map(int, input().split())
 graph = []
+
+visited = [[[0]*2 for _ in range(m)] for _ in range(n)]
+visited[0][0][0] = 1
+
 for i in range(n):
-    temp = list(input().strip())
-    graph.append(temp)
-
-dp = [[[0,0]]*m for i in range(n)]
-dp[0][0] = [1, 0]
-
-queue = deque()
-queue.append([0,0])
-
+    graph.append(input().strip())
 dx = [0,0,1,-1]
 dy = [1,-1,0,0]
 
-while queue:
-    tempx, tempy = queue.popleft()
-    for i in range(4):
-        x = tempx+dx[i]
-        y = tempy+dy[i]
-        if 0<=x<n and 0<=y<m:
-            if graph[x][y] == '0':
-                if dp[tempx][tempy][0] != 0:
-                    dp[x][y][0] = dp[tempx][tempy][0]+1
-                if dp[tempx][tempy][1] != 0:
-                    dp[x][y][1] = dp[tempx][tempy][1]+1
-                queue.append([x,y])
-                graph[x][y] = -1
-            if graph[x][y] == '1' and dp[tempx][tempy][1] == 0:
-                dp[x][y][0] = 0
-                dp[x][y][1] = dp[tempx][tempy][0] + 1
-                queue.append([x,y])
-                graph[x][y] = -1
-print(dp)
-if dp[n-1][m-1][0] == 0 and dp[n-1][m-1][1] == 0:
-    print(-1)
-elif dp[n-1][m-1][0] == 0:
-    print(dp[n-1][m-1][1])
-elif dp[n-1][m-1][1] == 0:
-    print(dp[n-1][m-1][0])
-else:
-    print(min(dp[n-1][m-1][0], dp[n-1][m-1][1]))
+def bfs(x,y,z):
+    queue = deque()
+    queue.append((x,y,z))
+    while queue:
+        a,b,c = queue.popleft()
+        if a==n-1 and b==m-1:
+            return visited[a][b][c]
+        for i in range(4):
+            nx = a + dx[i]
+            ny = b + dy[i]
+            if nx<0 or nx>=n or ny<0 or ny>=m:
+                continue
+            if graph[nx][ny] == '1' and c == 0:
+                visited[nx][ny][1] = visited[a][b][0] +1
+                queue.append((nx,ny,1))
+            elif graph[nx][ny] == '0' and visited[nx][ny][c] == 0:
+                visited[nx][ny][c] = visited[a][b][c] + 1
+                queue.append((nx,ny,c))
+    return -1
+
+print(bfs(0,0,0))
